bf08b79bfb9e320f6841671be22a8f3d8383b554
hipzi <52280647+hipzi@users.noreply.github.com>
2019-09-11 14:26:07 +0700

    Add files via upload

---
+++ roommaster.php
@@ -0,0 +1,39 @@
+<?php
+    include "bootstrap.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <!-- CSS -->
+        <style type="text/css">
+            @import url('style.css');
+        </style>
+
+        <title></title>
+    </head>
+
+    <body>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container">
+            <a class="navbar-brand" href="#">OnlineCinema</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+            </div>
+        </nav>
+
+        <!-- Jumbotron -->
+        <div class="jumbotron3 jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">WELCOME ROOM MASTER</h1>
+                <p> <a href="#" class="btn-upload">Upload File</a> </p>
+                <p> <a href="#" class="btn-update">Update</a> </p>
+            </div>
+        </div>
+
+    </body>
+</html>
---
+++ user.php
@@ -0,0 +1,38 @@
+<?php
+    include "bootstrap.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <!-- CSS -->
+        <style type="text/css">
+            @import url('style.css');
+        </style>
+
+        <title></title>
+    </head>
+
+    <body>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container">
+            <a class="navbar-brand" href="#">OnlineCinema</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+            </div>
+        </nav>
+
+        <!-- Jumbotron -->
+        <div class="jumbotron2 jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">WELCOME USER</h1>
+                <p> <a href="#" class="btn-join">Join</a> </p>
+            </div>
+        </div>
+
+    </body>
+</html>
