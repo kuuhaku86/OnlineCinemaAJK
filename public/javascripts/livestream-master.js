3cc72855c099ebad227013a82a72015644302282
hipzi <zahramillah582@gmail.com>
2019-10-16 22:59:51 +0700

    buat modal untuk lihat user yg online

--- public/javascripts/livestream-master.js
+++ public/javascripts/livestream-master.js
@@ -18,3 +18,18 @@ $(function(){
         viewVideo(video,context);
     },0);
 });
+
+var modal = document.getElementById('simpleModal');
+var modalBtn = document.getElementById('modalBtn');
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+
+function openModal() {
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+}
