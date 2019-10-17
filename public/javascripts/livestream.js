71492dd136bdf73ce2cd92fdc4aba3138a6035d9
hipzi <zahramillah582@gmail.com>
2019-10-17 17:19:10 +0700

    chat udah bisa pake enter + autoscroll 90%

--- public/javascripts/livestream.js
+++ public/javascripts/livestream.js
@@ -2,6 +2,20 @@ socket.on('message', function (data) {
     $('.chatlogs').append('<div class="chat"><p class="chat-message"><strong>' + data.user + '</strong>: ' + data.message + '</p></div>');
     });
     // When the form is submitted
+
+    // Get the input field
+    var input = document.getElementById('chat-input');
+
+    // Execute a function when the user releases a key on the keyboard
+    input.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById('button').click();
+    }
+
 $('#button').click(function (e) {
     // Avoid submitting it through HTTP
     e.preventDefault();
@@ -10,9 +24,37 @@ $('#button').click(function (e) {
     if(message == "")return;
     // Send the message to the server
     socket.emit('message', {
-        user: $('.nav-link').html(),
+        user: $('#navbarDropdown').html(),
         message: message
     });
     $('#chat-input').val('');
     // Clear the input and focus it for a new message
+
+    var message = document.getElementById('messages');
+
+    });
+    function appendMessage() {
+        var message = document.getElementById('chat-input');
+        var newMessage = message.cloneNode(true);
+        message.appendChild(newMessage);
+    }
+
+    function getMessages() {
+        // shouldScroll = messages.scrollTop + messages.clientHeight == messages.scrollHeight;
+        var scrollAmount = ($(document).innerHeight() - $(window).height()) * percentageToScroll / 100;
+        appendMessage();
+
+        if (!shouldScroll) {
+            scrollToBottom();
+        }
+    }
+
+    function scrollToBottom() {
+        messages.scrollTop = messages.scrollHeight;
+    }
+
+    scrollToBottom();
+
+    // setInterval(getMessages, 100);
+
     });
